9f14164d80b9fc8cfda80aec4affaef9e24e42fd
yh_zoo <79645167+Hyeok-Ju@users.noreply.github.com>
2023-08-08 14:58:48 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './css/app.css';
 
 
 const getRestData = dataName => {
-  const res = axios.get(`https://raw.githubusercontent.com/HNU209/Disabled_callTaxi_Simulation/main/src/data/${dataName}.json`);
+  const res = axios.get(`https://raw.githubusercontent.com/HNU209/Disabled-callTaxi_Simulation/main/src/data/${dataName}.json`);
   // const res = axios.get(`./data/${dataName}.json`);
   const result = res.then(r => r.data);
   return result;
